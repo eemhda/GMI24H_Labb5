bb610134f361a01f82eb38607333fc78d9670951
eem <eem@DUPC-722N1T3>
2023-05-03 13:53:11 +0200

    Add project files.

---
+++ GMI24H_Labb5_Grupp_ERTGRUPPNR/MyAlgorithmLibrary/Searcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMI24H_Labb5_Grupp_ERTGRUPPNR.MyAlgorithmLibrary
+{
+    /// <summary>
+    /// This class is used to implement the algorithms defined in the ISearcher interface.
+    /// When you have implemented the algorithms, you should be able to test them by instantiating
+    /// a Searcher object and call the methods.
+    /// </summary>
+    public class Searcher : ISearcher
+    {
+        public int BinarySearch(int[] array, int target)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public int ExponentialSearch(int[] array, int target)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public int InterpolationSearch(int[] array, int target)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public int JumpSearch(int[] array, int target)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public int LinearSearch(int[] array, int target)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GMI24H_Labb5_Grupp_ERTGRUPPNR/MyAlgorithmLibrary/Sorter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMI24H_Labb5_Grupp_ERTGRUPPNR.MyAlgorithmLibrary
+{
+    /// <summary>
+    /// This class is used to implement the algorithms defined in the ISorter interface.
+    /// When you have implemented the algorithms, you should be able to test them by instantiating
+    /// a Sorter object and call the methods.
+    /// </summary>
+    class Sorter : ISorter
+    {
+        public void BubbleSort(int[] arr)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public void HeapSort(int[] arr)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public void InsertionSort(int[] arr)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public void QuickSort(int[] arr, int low, int high)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+
+        public void SelectionSort(int[] arr)
+        {
+            //Replace the following code snippet with your implementation
+            throw new NotImplementedException();
+        }
+    }
+}
